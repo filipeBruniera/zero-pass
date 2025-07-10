52f42d1a90167613f7ebc15772095c43889a863e
Filipe Bruniera <81372107+filipeBruniera@users.noreply.github.com>
2025-07-10 14:00:16 -0300

    Add files via upload

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
+
+class Handler extends ExceptionHandler {
+    protected $levels = [];
+    protected $dontReport = [];
+    protected $dontFlash = ['password', 'password_confirmation'];
+
+    public function register() {
+        \$this->reportable(function (Throwable \$e) {});
+    }
+}
