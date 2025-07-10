02d7e5095c49d5048cddf9593fddfee1e54a73db
Filipe Bruniera <81372107+filipeBruniera@users.noreply.github.com>
2025-07-10 15:08:42 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
-# Gera chave da aplicação se necessário
 if [ ! -f ".env" ]; then
   cp .env.example .env
 fi
 
+php artisan key:generate --force
 php artisan config:clear
 php artisan config:cache
 php artisan migrate --force || true
+
+chmod -R 775 storage bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
 
 php artisan serve --host=0.0.0.0 --port=8000
