83c08774e4ccb65694764f71ad92a047db60ec9f
Filipe Bruniera <81372107+filipeBruniera@users.noreply.github.com>
2025-07-10 13:46:13 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+// start.sh (script de inicialização)
+#!/bin/sh
+set -e
+
+# Gera chave da aplicação se necessário
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
+php artisan config:clear
+php artisan config:cache
+php artisan migrate --force || true
+
+php artisan serve --host=0.0.0.0 --port=8000
